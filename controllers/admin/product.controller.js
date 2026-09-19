06a205ac6e6cdca69c9ba109bddfffd234f2c19d
Phùng Nhơn Hòa <nhonhoa2007@gmail.com>
2026-09-19 15:15:05 +0700

    filter on status

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,12 +3,47 @@ const Product = require("../../models/product.model");
 
 //GET /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    })
-    console.log(products)
+    //console.log(req.query.status);
+
+    let filterStatus = [
+        {
+            name: "All",
+            status: "",
+            class: "active",
+        },
+        {
+            name: "Active",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Inactive",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    let find = {
+        deleted: false,
+    }
+    if (req.query.status) {
+        find.status = req.query.status;
+    }
+    if (req.query.status) {
+        filterStatus.forEach(status => {
+            if (status.status === req.query.status) {
+                status.class = "active";
+            } else {
+                status.class = "inactive";
+            }
+        })
+    }
+    const products = await Product.find(find)
+
+    //console.log(products)
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
+        filterStatus: filterStatus
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,2 +1,22 @@
-// Admin JavaScript Logic
-console.log("Admin script loaded successfully!");
+//Button Status
+const buttonStatus = document.querySelectorAll("[button-status]");
+if (buttonStatus.length > 0) {
+
+
+    let url = new URL(window.location.href);
+    console.log(url)
+    buttonStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+
+            if (status) {
+                url.searchParams.set("status", status)
+            } else {
+                url.searchParams.delete("status");
+            }
+            console.log(url.href)
+            window.location.href = url.href;
+        })
+    })
+}
+//End Button Status
